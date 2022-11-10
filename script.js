d881844dbdfbe5fd89428925e38dfb86d03f5f4b
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-10 21:57:54 +0530

    add js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-<nav aria-label="...">
-  <ul class="pagination">
-    <li class="page-item disabled">
-      <a class="page-link">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active" aria-current="page">
-      <a class="page-link" href="#">2</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav>
+var nav=document.createElement("nav");
+nav.setAttribute("aria-label","...");
+
+var ul=document.createElement("ul");
+ul.setAttribute("class","pagination");
+
+var li=document.createElement("li");
+li.setAttribute("class","page-item disabled");
+
+var a=document.createElement("a");
+a.setAttribute("class","page-link");
+a.innerHTML="Previous";
+li.append(a);
+
+var li1=document.createElement("li1");
+li1.setAttribute("class","page-item");
+
+var a1=document.createElement("a");
+a1.setAttribute("class","page-link");
+a1.setAttribute("href","#");
+a1.innerHTML="1";
+li1.append(a1);
